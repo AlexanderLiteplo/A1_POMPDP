23c918f0b9bef852d90cffa1711ce8183b3e7231
Deep-Gill <54833651+Deep-Gill@users.noreply.github.com>
2022-09-30 03:04:03 -0700

    refactored code

--- src/com/company/RunSimulation.java
+++ src/com/company/RunSimulation.java
@@ -5,54 +5,11 @@ public class RunSimulation {
         try {
             StateType[][] map = new StateType[5][6];
             initializeMap(map);
-            State startingState = null;
-            int numOfActionsAndObservations = args.length / 2;
-            Action[] actions = new Action[numOfActionsAndObservations];
-            Observation[] observations = new Observation[numOfActionsAndObservations];
-            int isInitialStateSpecified = args.length % 2;
-            if (isInitialStateSpecified == 1) {
-                String startState = args[args.length - 1];
-                int row = Character.getNumericValue(startState.toCharArray()[1]);
-                int column = Character.getNumericValue(startState.toCharArray()[3]);
-                startingState = new State(row, column);
-            }
-
-            for (int i = 0; i < numOfActionsAndObservations; i++) {
-                String action = args[i].toLowerCase();
-                switch (action) {
-                    case "up":
-                        actions[i] = new Up();
-                        break;
-                    case "left":
-                        actions[i] =  new Left();
-                        break;
-                    case "down":
-                        actions[i] = new Down();
-                        break;
-                    case "right":
-                        actions[i] = new Right();
-                        break;
-                }
-            }
-
-            for (int i = 0; i < numOfActionsAndObservations; i++) {
-                String observation = args[i + numOfActionsAndObservations].toLowerCase();
-                switch (observation) {
-                    case "1wall":
-                        observations[i] = new Observation(ObsType.ONE_WALL);
-                        break;
-                    case "2walls":
-                        observations[i] = new Observation(ObsType.TWO_WALL);
-                        break;
-                    case "end":
-                        observations[i] = new Observation(ObsType.END);
-                        break;
-                }
-            }
-
+            State startingState = initializeStartingState(args);
+            Action[] actions = initializeActions(args);
+            Observation[] observations = initializeObservations(args);
             Agent agent = new Agent(map, startingState);
             agent.runPOMPD(actions, observations);
-
         } catch (Exception e) {
             System.out.println("Incorrect input. Please follow ReadMe instruction.");
             System.out.println("Error: " + e.getMessage());
@@ -79,6 +36,61 @@ public class RunSimulation {
             map[row][0] = StateType.WALL;
             map[row][5] = StateType.WALL;
         }
+    }
+
+    private static Action[] initializeActions(String[] args) {
+        int numOfActions = args.length / 2;
+        Action[] actions = new Action[numOfActions];
+        for (int i = 0; i < numOfActions; i++) {
+            String action = args[i].toLowerCase();
+            switch (action) {
+                case "up":
+                    actions[i] = new Up();
+                    break;
+                case "left":
+                    actions[i] =  new Left();
+                    break;
+                case "down":
+                    actions[i] = new Down();
+                    break;
+                case "right":
+                    actions[i] = new Right();
+                    break;
+            }
+        }
+        return actions;
+    }
+
+    private static Observation[] initializeObservations(String[] args) {
+        int numOfObservations = args.length / 2;
+        Observation[] observations = new Observation[numOfObservations];
+        for (int i = 0; i < numOfObservations; i++) {
+            String observation = args[i + numOfObservations].toLowerCase();
+            switch (observation) {
+                case "1wall":
+                    observations[i] = new Observation(ObsType.ONE_WALL);
+                    break;
+                case "2walls":
+                    observations[i] = new Observation(ObsType.TWO_WALL);
+                    break;
+                case "end":
+                    observations[i] = new Observation(ObsType.END);
+                    break;
+            }
+        }
+        return observations;
+    }
+
+    private static State initializeStartingState(String[] args) {
+        State startingState = null;
+        int isInitialStateSpecified = args.length % 2;
+        if (isInitialStateSpecified == 1) {
+            String startState = args[args.length - 1];
+            int row = Character.getNumericValue(startState.toCharArray()[1]);
+            int column = Character.getNumericValue(startState.toCharArray()[3]);
+            startingState = new State(row, column);
+        }
+        return startingState;
     }
 
 }
